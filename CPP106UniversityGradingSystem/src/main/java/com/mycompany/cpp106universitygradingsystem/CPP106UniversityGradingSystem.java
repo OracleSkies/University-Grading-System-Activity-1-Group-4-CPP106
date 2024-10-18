a77288db4a2eadd50ac062d0be96e10d329d0ec7
OracleSkies <153785033+OracleSkies@users.noreply.github.com>
2024-10-18 23:17:39 +0800

    Test Repo
    sheesh

---
+++ CPP106UniversityGradingSystem/src/main/java/com/mycompany/cpp106universitygradingsystem/CPP106UniversityGradingSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cpp106universitygradingsystem;
+
+/**
+ *
+ * @author Jayjay
+ */
+public class CPP106UniversityGradingSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
