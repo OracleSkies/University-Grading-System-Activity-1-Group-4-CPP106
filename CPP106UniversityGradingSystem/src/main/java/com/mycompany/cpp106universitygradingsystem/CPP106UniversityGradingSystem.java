56a456fe26ed41203837381a2ec52e4cfc82145c
adadsada <iangabuyo9@gmail.com>
2024-10-18 23:23:54 +0800

    hello po

--- CPP106UniversityGradingSystem/src/main/java/com/mycompany/cpp106universitygradingsystem/CPP106UniversityGradingSystem.java
+++ CPP106UniversityGradingSystem/src/main/java/com/mycompany/cpp106universitygradingsystem/CPP106UniversityGradingSystem.java
@@ -12,5 +12,6 @@ public class CPP106UniversityGradingSystem {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.err.println("hi po ");
     }
 }
