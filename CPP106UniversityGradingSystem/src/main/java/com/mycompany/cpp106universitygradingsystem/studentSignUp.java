7c06c6e0930cf2fd8fa386f4f7850feeda0ec94e
OracleSkies <153785033+OracleSkies@users.noreply.github.com>
2024-10-19 22:10:59 +0800

    debugged Login.java

--- CPP106UniversityGradingSystem/src/main/java/com/mycompany/cpp106universitygradingsystem/studentSignUp.java
+++ CPP106UniversityGradingSystem/src/main/java/com/mycompany/cpp106universitygradingsystem/studentSignUp.java
@@ -8,12 +8,12 @@ package com.mycompany.cpp106universitygradingsystem;
  *
  * @author Student
  */
-public class StudentSignUp extends javax.swing.JFrame {
+public class studentSignUp extends javax.swing.JFrame {
 
     /**
      * Creates new form SIGNUP
      */
-    public StudentSignUp() {
+    public studentSignUp() {
         initComponents();
     }
 
@@ -252,21 +252,27 @@ public class StudentSignUp extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(StudentSignUp.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(studentSignUp.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(StudentSignUp.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(studentSignUp.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(StudentSignUp.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(studentSignUp.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(StudentSignUp.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(studentSignUp.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
         //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new StudentSignUp().setVisible(true);
+                new studentSignUp().setVisible(true);
             }
         });
     }
